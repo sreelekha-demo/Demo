d39553f3bf5c382866061f577ec295cdf0e22bac
sreelekha-demo <sreelekha.sreekumar1105@gmail.com>
2026-06-03 11:52:55 +0530

    Remove target/classes from repo and add to .gitignore

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO employees(name, department, salary) VALUES ('John', 'IT', 60000);
-INSERT INTO employees(name, department, salary) VALUES ('David', 'HR', 45000);
-INSERT INTO employees(name, department, salary) VALUES ('Sarah', 'Finance', 80000);
-INSERT INTO employees(name, department, salary) VALUES ('Mike', 'IT', 75000);
